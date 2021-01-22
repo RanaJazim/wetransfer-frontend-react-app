7c2db9c4219f65b502d18941bdd01619c18e9c90
Jazim Abbas <stackdeveloper87@gmail.com>
2021-01-22 19:09:22 +0500

    added admin layout ..

---
+++ src/components/admin/admin_layout.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+const AdminLayout = ({ children }) => {
+  return (
+    <div>
+      <div className="sidebar">
+        <img className="logo2" src="/assets/images/Logo.png" width="200px" />
+        <br />
+        <br />
+        <img className="user1" src="/assets/images/user.png" width="50px" />
+        <div className="admin">
+          <b>Name Admin</b>
+          <br />
+          Administrator
+        </div>
+        <ul>
+          <li>
+            <a href="dashboard.html" className="active">
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="Events.html">Events</a>
+          </li>
+          <li>
+            <a href="AboutUs.html">About Us</a>
+          </li>
+          <li>
+            {" "}
+            <a href="Support.html">Support</a>
+          </li>
+          <li>
+            <a href="Contacts.html">Contacts</a>
+          </li>
+        </ul>
+      </div>
+
+      <div className="main">{children}</div>
+    </div>
+  );
+};
+
+export default AdminLayout;
