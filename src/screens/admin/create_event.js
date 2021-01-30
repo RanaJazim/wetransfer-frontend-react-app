c5642ca8518728a1d76602acb31be3132062f5b1
JazimAbbas <stackdeveloper87@gmail.com>
2021-01-30 22:24:52 +0500

    implemented validation and get the form values after successfull in admin create event screen ..

--- src/screens/admin/create_event.js
+++ src/screens/admin/create_event.js
@@ -29,8 +29,8 @@ const CreateEvent = () => {
 export default CreateEvent;
 
 function CreateEventForm() {
-  const handleSubmit = () => {
-    console.log("create event ..");
+  const handleSubmit = ({ formValues }) => {
+    console.log("create event ..", formValues);
   };
 
   return (
@@ -42,20 +42,61 @@ function CreateEventForm() {
       <div className="page-heading">
         <div className="patch" />
         <span className="page-name">Events</span>
+        <div className=" mt-5 mb-5">
+          <p>
+            <AppError field="image" />
+          </p>
+
+          <p>
+            <AppError field="title" />
+          </p>
+
+          <p>
+            <AppError field="description" />
+          </p>
+          <p>
+            <AppError field="applyDate" />
+          </p>
+          <p>
+            <AppError field="applyTime" />
+          </p>
+          <p>
+            <AppError field="eventDate" />
+          </p>
+          <p>
+            <AppError field="eventTime" />
+          </p>
+          <p>
+            <AppError field="location" />
+          </p>
+          <p>
+            <AppError field="shortenRoutes" />
+          </p>
+          <p>
+            <AppError field="priceToApply" />
+          </p>
+          <p>
+            <AppError field="federatedPrice" />
+          </p>
+          <p>
+            <AppError field="mealPrice" />
+          </p>
+        </div>
         <label>
           {" "}
           Choose File
-          <input type="file" size={60} />
+          <AppField field="image" type="file" size={60} />
         </label>
         <div>
           <br />
 
           <p>Event Name </p>
-          <input type="text" className="inputs" placeholder="Event Name" />
+          <AppField field="title" className="inputs" placeholder="Event Name" />
+
           <p>Description of Event</p>
-          <input
+          <AppField
+            field="description"
             className="input2 inputs"
-            type="text"
             placeholder="Enter Information"
           />
           <br />
@@ -63,40 +104,63 @@ function CreateEventForm() {
         <div className="leftline">
           <p>Limite Inscicao</p>
           <div className="form-inline">
-            <input type="text" placeholder="Selecionar Data" />
-            <input type="text" placeholder="Introduza Hora Limite" />
+            <AppField
+              field="applyDate"
+              type="date"
+              placeholder="Selecionar Data"
+            />
+            <AppField field="applyTime" placeholder="Introduza Hora Limite" />
           </div>
         </div>
         <div className="rightline">
           <p>Event Date</p>
           <div className="form-inline">
-            <input type="text" placeholder="Selecionar Data" />
-            <input type="text" placeholder=" Hora Evento" />
+            <AppField
+              field="eventDate"
+              type="date"
+              placeholder="Selecionar Data"
+            />
+            <AppField field="eventTime" placeholder="Hora Evento" />
           </div>
         </div>
         <div className="leftline">
           <p>Limite Inscicao</p>
           <div className="form-inline">
-            <input type="text" placeholder="Localizacao" />
+            <AppField field="location" placeholder="Localizacao" />
           </div>
         </div>
         <div className="rightline">
           <p>Percursos</p>
           <div className="form-inline">
-            <input type="text" placeholder="Intorduzir Percursos" />
+            <AppField
+              field="shortenRoutes"
+              placeholder="Intorduzir Percursos"
+            />
           </div>
         </div>
         <div className="leftline">
           <p>Price to Apply</p>
           <div className="form-inline">
-            <input type="text" placeholder="Selecionar Data" />
+            <AppField
+              field="priceToApply"
+              type="number"
+              placeholder="Price to Apply"
+            />
           </div>
         </div>
         <div className="rightline">
           <p>Price</p>
           <div className="form-inline">
-            <input type="text" placeholder="Meal Price" />
-            <input type="text" placeholder="Federated Price" />
+            <AppField
+              field="mealPrice"
+              type="number"
+              placeholder="Meal Price"
+            />
+            <AppField
+              field="federatedPrice"
+              type="number"
+              placeholder="Federated Price"
+            />
           </div>
         </div>
         <button className="left-corner" type="submit">
