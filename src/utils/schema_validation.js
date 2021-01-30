db1c393e1684637ef9c90b599ad9289c8cb5966b
JazimAbbas <stackdeveloper87@gmail.com>
2021-01-31 00:01:36 +0500

    added appform to register-event screen ..

--- src/utils/schema_validation.js
+++ src/utils/schema_validation.js
@@ -20,3 +20,19 @@ export const createAboutSchema = Yup.object({
   title: Yup.string().required(),
   description: Yup.string().required(),
 });
+
+export const registerEventSchema = Yup.object({
+  email: Yup.string().email().required(),
+  dateOfBirth: Yup.date().required(),
+  gender: Yup.string().required(),
+  phone: Yup.string().required(),
+  address: Yup.string().required(),
+  door: Yup.string().required(),
+  floor: Yup.string().required(),
+  zipCode: Yup.string().required(),
+  nif: Yup.string().required(),
+  category: Yup.string().required(),
+  selectedEvent: Yup.string().required(),
+  companions: Yup.number().required(),
+  issue: Yup.string().notRequired(),
+});
