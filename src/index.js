67caf3f1cedb8c45adb6ac7f7f51199eeb795bb5
JazimAbbas <stackdeveloper87@gmail.com>
2021-01-31 09:47:27 +0500

    added axios and nprogresss ..

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import "nprogress/nprogress.css";
 
 import App from "./App";
 
