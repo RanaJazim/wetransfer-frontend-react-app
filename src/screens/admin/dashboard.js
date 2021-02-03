b1cd946fdae700134684025fddcbb580df2e30a1
JazimAbbas <stackdeveloper87@gmail.com>
2021-02-03 12:14:48 +0500

    moved graph summary to its own component ..

--- src/screens/admin/dashboard.js
+++ src/screens/admin/dashboard.js
@@ -6,22 +6,6 @@ import * as eventService from "../../services/event";
 import { AdminLayout } from "../../components/admin";
 
 const DashboardScreen = () => {
-  const data = [
-    ["Month", "Revenue", { role: "style" }],
-    ["Jan", 8, "#2c8850"],
-    ["Feb", 10, "#2c8850"],
-    ["Mar", 19, "#2c8850"],
-    ["Apr", 21, "#2c8850"],
-    ["May", 23, "#2c8850"],
-    ["Jun", 32, "#2c8850"],
-    ["Jul", 1, "#2c8850"],
-    ["Aug", 2, "#2c8850"],
-    ["Sep", 54, "#2c8850"],
-    ["Oct", 23, "#2c8850"],
-    ["Nov", 54, "#2c8850"],
-    ["Dec", 25, "#2c8850"],
-  ];
-
   return (
     <AdminLayout>
       <div className="">
@@ -46,71 +30,7 @@ const DashboardScreen = () => {
           {/* Begin Page Content */}
           <div className="container-fluid">
             {/* Content Row */}
-            <div className="row">
-              {/*Valor Total */}
-              <div className="col-xl-3 col-md-6 mb-4">
-                <div
-                  className="card shadow h-100 py-2"
-                  style={{ background: "#2c8850", color: "#fff" }}
-                >
-                  <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                      <div className="col mr-2">
-                        <div className="text-xs font-weight-bold  text-uppercase mb-1">
-                          Valor Total
-                        </div>
-                        <div className="h5 mb-0 font-weight-bold">$40,000</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <br />
-                <div
-                  className="card shadow h-100 py-2"
-                  style={{ background: "#2c8850", color: "#fff" }}
-                >
-                  <div className="card-body">
-                    <div className="row no-gutters align-items-center">
-                      <div className="col mr-2">
-                        <div className="text-xs font-weight-bold  text-uppercase mb-1">
-                          Inscripto
-                        </div>
-                        <div className="h5 mb-0 font-weight-bold">500</div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              {/* Pie Chart */}
-              <div className="col-xl-4 col-md-6 mb-4">
-                <div className="card shadow mb-4">
-                  <p>Inscriptions</p>
-                  <Chart
-                    chartType="PieChart"
-                    loader={<div>Loading Chart ...</div>}
-                    data={[
-                      ["Task", "Hours per Day"],
-                      ["Male", 11],
-                      ["Female", 2],
-                    ]}
-                    rootProps={{ "data-testid": "1" }}
-                  />
-                </div>
-              </div>
-              <br />
-              <br />
-              <br />
-              <div className="col-xl-4 col-md-6 mb-4">
-                <div className="card shadow mb-4">
-                  <p>Revenue</p>
-                  <Chart
-                    chartType="ColumnChart"
-                    loader={<div>Loading Chart ...</div>}
-                    data={data}
-                  />
-                </div>
-              </div>
-            </div>
+            <RegistrationSummary />
           </div>
           <br />
 
@@ -122,6 +42,84 @@ const DashboardScreen = () => {
 };
 
 export default DashboardScreen;
+
+function RegistrationSummary() {
+  const data = [
+    ["Age", "No. of People"],
+    ["<25", 2],
+    ["25-35", 1],
+    ["36-50", 5],
+    [">50", 0],
+  ];
+
+  return (
+    <div className="row">
+      {/*Valor Total */}
+      <div className="col-xl-3 col-md-6 mb-4">
+        <div
+          className="card shadow h-100 py-2"
+          style={{ background: "#2c8850", color: "#fff" }}
+        >
+          <div className="card-body">
+            <div className="row no-gutters align-items-center">
+              <div className="col mr-2">
+                <div className="text-xs font-weight-bold  text-uppercase mb-1">
+                  Valor Total
+                </div>
+                <div className="h5 mb-0 font-weight-bold">$40,000</div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div
+          className="card shadow h-100 py-2"
+          style={{ background: "#2c8850", color: "#fff" }}
+        >
+          <div className="card-body">
+            <div className="row no-gutters align-items-center">
+              <div className="col mr-2">
+                <div className="text-xs font-weight-bold  text-uppercase mb-1">
+                  Inscripto
+                </div>
+                <div className="h5 mb-0 font-weight-bold">500</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Pie Chart */}
+      <div className="col-xl-4 col-md-6 mb-4">
+        <div className="card shadow mb-4">
+          <p>Inscriptions</p>
+          <Chart
+            chartType="PieChart"
+            loader={<div>Loading Chart ...</div>}
+            data={[
+              ["Task", "Hours per Day"],
+              ["Male", 11],
+              ["Female", 2],
+            ]}
+            rootProps={{ "data-testid": "1" }}
+          />
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <div className="col-xl-4 col-md-6 mb-4">
+        <div className="card shadow mb-4">
+          <p>No of people register w.r.t to age</p>
+          <Chart
+            chartType="ColumnChart"
+            loader={<div>Loading Chart ...</div>}
+            data={data}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
 
 function EventRegistrations() {
   const eventApi = useApi(eventService.allRegistrationsForEvent);
