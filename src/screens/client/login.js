73dc24c7c47b3d6aedf9f21b51b439d040d7af50
JazimAbbas <stackdeveloper87@gmail.com>
2021-02-03 14:52:39 +0500

    moved login form to its own component ..

--- src/screens/client/login.js
+++ src/screens/client/login.js
@@ -7,39 +7,41 @@ const LoginScreen = () => {
       <div className="loginBox">
         <img className="user" src="/assets/images/profile-user.png" />
         <p>Log in Administrator</p>
-        <form action="dashboard.html">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Username"
-            />
-            <div className="input-group-append">
-              <i
-                className="fa fa-user p-2"
-                style={{ backgroundColor: "rgb(214 214 214)" }}
-              ></i>
-            </div>
-          </div>
-          <div className="input-group my-4">
-            <input
-              type="password"
-              className="form-control"
-              placeholder="Password"
-            />
-            <div className="input-group-append">
-              <i
-                className="fa fa-key p-2"
-                style={{ backgroundColor: "rgb(214 214 214)" }}
-              ></i>
-            </div>
-          </div>
-
-          <button>Log in</button>
-        </form>
+        <LoginForm />
       </div>
     </div>
   );
 };
 
 export default LoginScreen;
+
+function LoginForm() {
+  return (
+    <form action="dashboard.html">
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="Username" />
+        <div className="input-group-append">
+          <i
+            className="fa fa-user p-2"
+            style={{ backgroundColor: "rgb(214 214 214)" }}
+          ></i>
+        </div>
+      </div>
+      <div className="input-group my-4">
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+        />
+        <div className="input-group-append">
+          <i
+            className="fa fa-key p-2"
+            style={{ backgroundColor: "rgb(214 214 214)" }}
+          ></i>
+        </div>
+      </div>
+
+      <button>Log in</button>
+    </form>
+  );
+}
